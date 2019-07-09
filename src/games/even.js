b54ec351f6c9285b54570ea6829774f56f17776b
Vadim S <vs1905@gmail.com>
2019-07-09 08:58:03 +0300

    move const outside the function

--- src/games/even.js
+++ src/games/even.js
@@ -1,9 +1,9 @@
 import gameEngine from '..';
 
 console.log('Answer "yes" if number even otherwise answer "no".');
+const isEven = int => int % 2 === 0;
 const startGame = () => {
   const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
-  const isEven = int => int % 2 === 0;
   const number = getRandomInt(1, 1000);
 
   let output = [];
