6a505bda2c5652cb206f1190b3fd01feb3f588b7
Vadim S <vs1905@gmail.com>
2019-07-15 00:43:33 +0300

    use array for operators

--- src/games/calc.js
+++ src/games/calc.js
@@ -5,16 +5,19 @@ const gameCondition = 'What is the result of the expression?';
 const startGame = () => {
   const firstOperand = getRandomNumber(1, 10);
   const secondOperand = getRandomNumber(1, 10);
-  const operatorsCount = 3;
+  const questions = [String(firstOperand - secondOperand), String(firstOperand + secondOperand),
+    String(firstOperand * secondOperand)];
+  const operatorsCount = questions.length;
+  const [minus, plus, multiple] = questions;
   const operatorChoice = getRandomNumber(0, operatorsCount);
 
   switch (operatorChoice) {
     case 0:
-      return [`${firstOperand} - ${secondOperand}`, String(firstOperand - secondOperand)];
+      return [`${firstOperand} - ${secondOperand}`, minus];
     case 1:
-      return [`${firstOperand} + ${secondOperand}`, String(firstOperand + secondOperand)];
+      return [`${firstOperand} + ${secondOperand}`, plus];
     case 2:
-      return [`${firstOperand} * ${secondOperand}`, String(firstOperand * secondOperand)];
+      return [`${firstOperand} * ${secondOperand}`, multiple];
     default:
       return [];
   }
