e54e30ce9b2e9e9b95cc59f3e3a81e48677edaf2
Vadim S <vs1905@gmail.com>
2019-07-15 00:45:57 +0300

    move the function outside

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -2,15 +2,17 @@ import gameEngine from '..';
 import getRandomNumber from '../generator';
 
 const gameCondition = 'Find the greatest common divisor of given numbers.';
+const findGcd = (a, b) => {
+  if (b === 0) {
+    return a;
+  }
+  return findGcd(b, a % b);
+};
+
 const startGame = () => {
   const random1 = getRandomNumber(1, 21);
   const random2 = getRandomNumber(1, 21);
-  const findGcd = (a, b) => {
-    if (b === 0) {
-      return a;
-    }
-    return findGcd(b, a % b);
-  };
+
   const gcd = findGcd(random1, random2);
 
   return [`${random1} ${random2}`, String(gcd)];
