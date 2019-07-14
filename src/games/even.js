65079226c1558315e7bb5b39c25f02b7bb72da35
Vadim S <vs1905@gmail.com>
2019-07-15 00:13:00 +0300

    refactor: use ternary operator

--- src/games/even.js
+++ src/games/even.js
@@ -5,14 +5,8 @@ const gameCondition = 'Answer "yes" if number even otherwise answer "no".';
 const isEven = int => int % 2 === 0;
 const startGame = () => {
   const question = getRandomNumber(1, 1000);
-
-  let output = '';
-  if (isEven(question)) {
-    output = 'yes';
-  } else {
-    output = 'no';
-  }
-  return [`${question}`, output];
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
+  return [`${question}`, correctAnswer];
 };
 
 gameEngine(gameCondition, startGame);
