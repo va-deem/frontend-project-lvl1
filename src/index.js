30f5c1470dd3562224abb09670b2aea65c318927
Vadim S <vs1905@gmail.com>
2019-07-17 09:45:27 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -5,27 +5,21 @@ const gameEngine = (condition, func) => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   console.log(condition);
-  const check = () => {
-    const [question, correctAnswer] = func();
-    const answer = readlineSync.question(`Question: ${question}\nYour answer:`);
-    let result = '';
-    if (answer === correctAnswer) {
-      console.log('Correct!');
-      result = true;
-    } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was ${correctAnswer}. \nLet's try again, ${name}!`);
-      result = false;
-    }
-    return result;
-  };
 
   let answersCount = 0;
   const attemptsNumber = 3;
   for (let i = 0; i < attemptsNumber; i += 1) {
-    if (check()) {
+    const [question, correctAnswer] = func();
+    const answer = readlineSync.question(`Question: ${question}\nYour answer:`);
+    if (answer === correctAnswer) {
+      console.log('Correct!');
       answersCount += 1;
-    } else break;
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was ${correctAnswer}. \nLet's try again, ${name}!`);
+      break;
+    }
   }
+
   if (answersCount === attemptsNumber) {
     console.log(`Congratulations, ${name}!`);
   }
