9e07a5ef357cecd185f2865ca9b74b9846237bd7
Vadim S <vs1905@gmail.com>
2019-07-20 11:31:06 +0300

    refactor

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,17 +2,16 @@ import gameEngine from '..';
 import getRandomNumber from '../generator';
 
 const gameCondition = 'What number is missing in the progression?';
+const progressionLength = 10;
 const generateGame = () => {
   const randomStart = getRandomNumber(1, 100);
   const randomStep = getRandomNumber(3, 7);
-  const progressionLength = 10;
   const randomElement = getRandomNumber(1, progressionLength - 1);
 
-  let element = randomStart;
   let sequence = '';
   let missingElement = 0;
   for (let i = 0; i < progressionLength; i += 1) {
-    element = randomStart + randomStep * i;
+    const element = randomStart + randomStep * i;
     if (i === randomElement) {
       sequence = `${sequence} ..`;
       missingElement = element;
