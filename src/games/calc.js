de2a0ed9cdcd3cd612f94199857074f5fbbdf1d5
Vadim S <vs1905@gmail.com>
2019-07-20 11:24:56 +0300

    refactor

--- src/games/calc.js
+++ src/games/calc.js
@@ -5,19 +5,21 @@ const gameCondition = 'What is the result of the expression?';
 const generateGame = () => {
   const firstOperand = getRandomNumber(1, 10);
   const secondOperand = getRandomNumber(1, 10);
-  const questions = [String(firstOperand - secondOperand), String(firstOperand + secondOperand),
-    String(firstOperand * secondOperand)];
-  const operatorsCount = questions.length;
-  const [minus, plus, multiple] = questions;
-  const operatorChoice = getRandomNumber(0, operatorsCount - 1);
 
+
+  const minus = [`${firstOperand} - ${secondOperand}`, String(firstOperand - secondOperand)];
+  const plus = [`${firstOperand} + ${secondOperand}`, String(firstOperand + secondOperand)];
+  const multiple = [`${firstOperand} * ${secondOperand}`, String(firstOperand * secondOperand)];
+  const questions = [minus, plus, multiple];
+
+  const operatorChoice = getRandomNumber(0, questions.length - 1);
   switch (operatorChoice) {
     case 0:
-      return [`${firstOperand} - ${secondOperand}`, minus];
+      return questions[0];
     case 1:
-      return [`${firstOperand} + ${secondOperand}`, plus];
+      return questions[1];
     case 2:
-      return [`${firstOperand} * ${secondOperand}`, multiple];
+      return questions[2];
     default:
       return [];
   }
