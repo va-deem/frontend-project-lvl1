5819353a045e6f48784926ecd5f708986c94b73b
Vadim S <vs1905@gmail.com>
2019-07-23 13:00:15 +0300

    refactor

--- src/games/calc.js
+++ src/games/calc.js
@@ -5,7 +5,6 @@ const gameCondition = 'What is the result of the expression?';
 const generateGame = () => {
   const firstOperand = getRandomNumber(1, 10);
   const secondOperand = getRandomNumber(1, 10);
-
   const operators = ['-', '+', '*'];
   const randomOperator = operators[getRandomNumber(0, operators.length - 1)];
   const question = `${firstOperand} ${randomOperator} ${secondOperand}`;
